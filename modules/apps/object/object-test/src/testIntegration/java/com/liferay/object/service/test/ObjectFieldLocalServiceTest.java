3b86fed4c0142438f1b839121e8655812ce999a9
Jerry Adriano <jerry.adriano@liferay.com>
2025-11-03 11:24:45 -0300

    Revert "LPD-61657 Add test for checking new ObjectField ResourcePermission"
    This reverts commit cfd1be7702084b9683c53a1234e4d20c2a92e057.

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectFieldLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectFieldLocalServiceTest.java
@@ -78,7 +78,6 @@ import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
-import com.liferay.portal.kernel.service.ResourceActionLocalService;
 import com.liferay.portal.kernel.test.AssertUtils;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -94,7 +93,6 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.SystemProperties;
 import com.liferay.portal.kernel.util.TempFileEntryUtil;
 import com.liferay.portal.kernel.util.TextFormatter;
-import com.liferay.portal.language.override.service.PLOEntryLocalService;
 import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -1422,7 +1420,6 @@ public class ObjectFieldLocalServiceTest {
 			modifiableSystemObjectDefinition);
 	}
 
-	@FeatureFlag("LPD-17564")
 	@Test
 	public void testDeleteObjectField() throws Exception {
 
@@ -1564,20 +1561,6 @@ public class ObjectFieldLocalServiceTest {
 					).build())
 			).build());
 
-		String actionId =
-			attachmentObjectField.getAttachmentDownloadActionKey();
-
-		ObjectDefinition objectDefinition =
-			attachmentObjectField.getObjectDefinition();
-
-		Assert.assertNotNull(
-			_resourceActionLocalService.fetchResourceAction(
-				objectDefinition.getClassName(), actionId));
-		Assert.assertNotNull(
-			_ploEntryLocalService.fetchPLOEntry(
-				objectDefinition.getCompanyId(), "action." + actionId,
-				attachmentObjectField.getDefaultLanguageId()));
-
 		ObjectEntry objectEntry = _objectEntryLocalService.addObjectEntry(
 			0, TestPropsValues.getUserId(),
 			customObjectDefinition.getObjectDefinitionId(),
@@ -1614,13 +1597,6 @@ public class ObjectFieldLocalServiceTest {
 				"No FileEntry exists with the key {fileEntryId=",
 				persistedFileEntryId, "}"),
 			() -> _dlAppLocalService.getFileEntry(persistedFileEntryId));
-		Assert.assertNull(
-			_resourceActionLocalService.fetchResourceAction(
-				objectDefinition.getClassName(), actionId));
-		Assert.assertNull(
-			_ploEntryLocalService.fetchPLOEntry(
-				objectDefinition.getCompanyId(), "action." + actionId,
-				attachmentObjectField.getDefaultLanguageId()));
 
 		// Delete object field business type auto increment
 
@@ -3023,9 +2999,6 @@ public class ObjectFieldLocalServiceTest {
 	@Inject
 	private ObjectRelationshipLocalService _objectRelationshipLocalService;
 
-	@Inject
-	private PLOEntryLocalService _ploEntryLocalService;
-
 	private final Map<String, String> _readOnlyObjectFieldDBTypes =
 		HashMapBuilder.put(
 			"createDate", ObjectFieldConstants.DB_TYPE_DATE
@@ -3038,8 +3011,5 @@ public class ObjectFieldLocalServiceTest {
 		).put(
 			"status", ObjectFieldConstants.DB_TYPE_INTEGER
 		).build();
-
-	@Inject
-	private ResourceActionLocalService _resourceActionLocalService;
 
 }
