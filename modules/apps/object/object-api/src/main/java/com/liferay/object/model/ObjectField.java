6d6a7942fd04aed363b9fae87328f508ba4f6401
Jerry Adriano <jerry.adriano@liferay.com>
2025-11-03 11:26:38 -0300

    Revert "LPD-61657 gw buildService"
    This reverts commit f1e8c359924be11627dd39117a11936fd4c0c170.

--- modules/apps/object/object-api/src/main/java/com/liferay/object/model/ObjectField.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/model/ObjectField.java
@@ -49,8 +49,6 @@ public interface ObjectField extends ObjectFieldModel, PersistedModel {
 
 	public boolean compareBusinessType(String businessType);
 
-	public String getAttachmentDownloadActionKey();
-
 	public String[] getDBColumnNames();
 
 	public String getI18nObjectFieldName();
--- modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectFieldLocalService.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectFieldLocalService.java
@@ -98,10 +98,6 @@ public interface ObjectFieldLocalService
 			List<ObjectFieldSetting> objectFieldSettings)
 		throws PortalException;
 
-	public void addOrUpdateObjectFieldResourceActionPLOEntries(
-			ObjectField objectField)
-		throws PortalException;
-
 	@Indexable(type = IndexableType.REINDEX)
 	public ObjectField addOrUpdateSystemObjectField(
 			String externalReferenceCode, long userId,
@@ -403,10 +399,6 @@ public interface ObjectFieldLocalService
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public Map<Long, List<ObjectField>> getObjectFieldsMap(long companyId);
-
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public Map<Long, List<ObjectField>> getObjectFieldsMap(
-		long companyId, String businessType);
 
 	/**
 	 * Returns the OSGi service identifier.
