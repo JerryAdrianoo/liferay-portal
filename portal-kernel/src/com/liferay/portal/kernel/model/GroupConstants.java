683b6b639ad50e23b5f6e52cf6cd680d4059f5ff
Jerry Adriano <ajerry550@gmail.com>
2026-04-14 09:49:44 -0300

    LPD-84569 followup

--- portal-kernel/src/com/liferay/portal/kernel/model/GroupConstants.java
+++ portal-kernel/src/com/liferay/portal/kernel/model/GroupConstants.java
@@ -57,7 +57,7 @@ public class GroupConstants {
 	public static final String SEO_STUDIO_FRIENDLY_URL = "/seo-studio";
 
 	public static final String[] SYSTEM_GROUPS = {
-		CALENDAR, CMS, CONTROL_PANEL, FORMS, GUEST,
+		CALENDAR, CMS, CONTROL_PANEL, FORMS, GUEST, SEO_STUDIO,
 		GroupConstants.USER_PERSONAL_SITE
 	};
 
