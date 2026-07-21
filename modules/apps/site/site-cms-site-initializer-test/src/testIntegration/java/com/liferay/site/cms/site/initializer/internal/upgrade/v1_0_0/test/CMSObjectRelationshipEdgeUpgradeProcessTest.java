1d83bdd56a6b1779e84aa31fe84fe5e2982f92c1
Jerry Adriano <ajerry550@gmail.com>
2026-07-21 15:59:55 -0300

    Revert "LPD-95465 SF"
    This reverts commit 59f60672747f7d3b5d9b5fec361991ee7e22f7b4.

--- modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/upgrade/v1_0_0/test/CMSObjectRelationshipEdgeUpgradeProcessTest.java
+++ modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/upgrade/v1_0_0/test/CMSObjectRelationshipEdgeUpgradeProcessTest.java
@@ -250,36 +250,7 @@ public class CMSObjectRelationshipEdgeUpgradeProcessTest {
 	public void testUpgradeObjectRelationshipsCascadeDeletionType()
 		throws Exception {
 
-		ObjectDefinition objectDefinition1 = _addCMSObjectDefinition();
-		ObjectDefinition objectDefinition2 = _addCMSObjectDefinition();
-		ObjectDefinition objectDefinition3 = _addCMSObjectDefinition();
-		ObjectDefinition objectDefinition4 = _addCMSObjectDefinition();
-
-		ObjectRelationship objectRelationship1 = _addObjectRelationship(
-			objectDefinition2, objectDefinition1);
-		ObjectRelationship objectRelationship2 = _addObjectRelationship(
-			objectDefinition3, objectDefinition1);
-		ObjectRelationship objectRelationship3 = _addObjectRelationship(
-			objectDefinition4, objectDefinition2);
-		ObjectRelationship objectRelationship4 = _addObjectRelationship(
-			objectDefinition4, objectDefinition3);
-
-		_runUpgrade();
-
-		_assertObjectRelationshipEdge(
-			true, objectRelationship1.getObjectRelationshipId());
-		_assertObjectRelationshipEdge(
-			true, objectRelationship2.getObjectRelationshipId());
-		_assertObjectRelationshipEdge(
-			true, objectRelationship3.getObjectRelationshipId());
-		_assertObjectRelationshipEdge(
-			true, objectRelationship4.getObjectRelationshipId());
-
-		long objectDefinitionId1 = objectDefinition1.getObjectDefinitionId();
-
-		_assertRootObjectDefinitionIds(objectDefinitionId1, objectDefinition2);
-		_assertRootObjectDefinitionIds(objectDefinitionId1, objectDefinition3);
-		_assertRootObjectDefinitionIds(objectDefinitionId1, objectDefinition4);
+		_testUpgradeObjectRelationshipsCascadeDeletionType();
 	}
 
 	@Test
@@ -541,6 +512,41 @@ public class CMSObjectRelationshipEdgeUpgradeProcessTest {
 			_upgradeStepRegistrator, _CLASS_NAME);
 
 		upgradeProcess.upgrade();
+	}
+
+	private void _testUpgradeObjectRelationshipsCascadeDeletionType()
+		throws Exception {
+
+		ObjectDefinition objectDefinition1 = _addCMSObjectDefinition();
+		ObjectDefinition objectDefinition2 = _addCMSObjectDefinition();
+		ObjectDefinition objectDefinition3 = _addCMSObjectDefinition();
+		ObjectDefinition objectDefinition4 = _addCMSObjectDefinition();
+
+		ObjectRelationship objectRelationship1 = _addObjectRelationship(
+			objectDefinition2, objectDefinition1);
+		ObjectRelationship objectRelationship2 = _addObjectRelationship(
+			objectDefinition3, objectDefinition1);
+		ObjectRelationship objectRelationship3 = _addObjectRelationship(
+			objectDefinition4, objectDefinition2);
+		ObjectRelationship objectRelationship4 = _addObjectRelationship(
+			objectDefinition4, objectDefinition3);
+
+		_runUpgrade();
+
+		_assertObjectRelationshipEdge(
+			true, objectRelationship1.getObjectRelationshipId());
+		_assertObjectRelationshipEdge(
+			true, objectRelationship2.getObjectRelationshipId());
+		_assertObjectRelationshipEdge(
+			true, objectRelationship3.getObjectRelationshipId());
+		_assertObjectRelationshipEdge(
+			true, objectRelationship4.getObjectRelationshipId());
+
+		long objectDefinitionId1 = objectDefinition1.getObjectDefinitionId();
+
+		_assertRootObjectDefinitionIds(objectDefinitionId1, objectDefinition2);
+		_assertRootObjectDefinitionIds(objectDefinitionId1, objectDefinition3);
+		_assertRootObjectDefinitionIds(objectDefinitionId1, objectDefinition4);
 	}
 
 	private static final String _CLASS_NAME =
